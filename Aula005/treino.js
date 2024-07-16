1fb52855f72187580e46b26e6b5070de33a7eca7
Wellington <161943926+WellingtonDeLacerda@users.noreply.github.com>
2024-07-16 07:46:40 -0300

    Add files via upload

---
+++ Aula005/treino.js
@@ -0,0 +1,15 @@
+let zoro = {
+    nome : "zoro",
+    idade : 21
+}
+let objetos ={
+    cor : ["azul","vermelho","verde"]
+}
+objetos.coisas = []
+objetos.coisas = [...objetos.coisas,"sla",67,"vixi764"]
+console.log(objetos.cor)
+console.log(objetos.coisas)
+
+
+
+
